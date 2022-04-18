efc942aaf3dc09e3125733902f0828edfe50d6ae
Luksh <hotmale_luksh@hotmail.com>
2022-04-18 09:03:21 +0545

    third commit

--- home/views.py
+++ home/views.py
@@ -4,5 +4,17 @@ app_name = "home"
 
 # Create your views here.
 
+class BaseView(View):
+    view = {}
+
+class HomeView(BaseView):
+    def get(self, request):
+        self.view['categories'] = Category.objects.all()
+        self.view['subcategories'] = SubCategory.objects.all()
+        self.view['sliders'] = Slider.objects.all()
+        self.view['ads'] = Ad.objects.all()
+        self.view['products'] = Product.objects.filter(status= 'active')
+
+        return render(request, 'index-4.html', self.view)
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index-4.html')
