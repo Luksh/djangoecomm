4703f0008bd301c0d5742b9bd6de9d8c74c575c1
Luksh <hotmale_luksh@hotmail.com>
2022-04-24 09:24:47 +0545

    eighth commit

--- home/models.py
+++ home/models.py
@@ -55,11 +55,14 @@ class Contact(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length= 400)
     image = models.ImageField(upload_to = 'media')
+    price = models.IntegerField(default= 0)
     discounted_price = models.IntegerField()
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
     subcategory = models.ForeignKey(SubCategory, on_delete = models.CASCADE)
     status = models.CharField(max_length= 50, choices= (('active','active'),('inactive','inactive')))
     labels = models.CharField(max_length= 50, choices= LABELS)
+    slug = models.CharField(max_length= 400, blank= True)
+    description = models.TextField(blank= True)
 
     def __str__(self):
         return self.name
