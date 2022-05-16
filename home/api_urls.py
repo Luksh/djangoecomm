0cce981114e102a1cf1cfc7f11431422c7533d06
Luksh <hotmale_luksh@hotmail.com>
2022-05-16 09:20:00 +0545

    crud

--- home/api_urls.py
+++ home/api_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
-from .views import ProductViewSet, ProductFilterView
+from .views import ProductCRUDViewSet, ProductViewSet, ProductFilterView
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -10,4 +10,5 @@ router.register(r'products', ProductViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('product_filter', ProductFilterView.as_view(), name='product_filter'),
+    path('product_crud/<int:pk>', ProductCRUDViewSet.as_view(), name='product_crud'),
 ]
